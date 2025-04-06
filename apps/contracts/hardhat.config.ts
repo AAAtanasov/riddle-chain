89ee624ce4cbfe81daecbe04507080c7530f7ed3
Anton Atanasov <atanassov1337@gmail.com>
2025-04-06 15:00:23 +0300

    feat(contracts-setup): PR refinements

--- apps/contracts/hardhat.config.ts
+++ apps/contracts/hardhat.config.ts
@@ -7,18 +7,11 @@ dotenvConfig({ path: __dirname + "/.env.local" });
 
 const { DEPLOYER_PRIVATE_KEY, INFURA_API_KEY, PRIVATE_KEY } = process.env;
 
-// if (!DEPLOYER_PRIVATE_KEY) {
-//   throw new Error("DEPLOYER_PRIVATE_KEY is not defined");
-// }
-
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
   networks: {
     hardhat: {
       chainId: 1337,
-      // accounts: [
-      //   { privateKey: DEPLOYER_PRIVATE_KEY || "", balance: "10000000000000000000000" },
-      // ]
     },
     sepolia: {
       url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
